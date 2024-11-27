f320bd10e24fb50590300b96f50baf1f884a729a
Vitalii <saifutdinov_vitalii@mail.ru>
2024-11-27 07:12:49 +0500

    Сделал секцию с скрабами

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
-let splide = new Splide( '.splide' );
-splide.mount();
+// new Splide('.splide').mount();
+// new Splide('.splide2').mount();
+
+var elms = document.getElementsByClassName( 'splide' );
+
+for ( var i = 0; i < elms.length; i++ ) {
+  new Splide( elms[ i ] ).mount();
+}
